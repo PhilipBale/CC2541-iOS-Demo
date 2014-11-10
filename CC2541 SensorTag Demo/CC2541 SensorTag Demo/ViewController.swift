d519712d0cc5b5746b68c68c5bad6c9bafd02540
Philip Bale <pbale95@gmail.com>
2014-11-09 22:04:29 -0500

    Adding initial project files

---
+++ CC2541 SensorTag Demo/CC2541 SensorTag Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CC2541 SensorTag Demo
+//
+//  Created by Philip Bale on 11/9/14.
+//  Copyright (c) 2014 Philip Bale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
